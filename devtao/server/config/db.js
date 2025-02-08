3b636e5575e96287f3ff7f269763d014bebce35c
Richard Li <richardjiali674@gmail.com>
2025-02-08 17:13:06 -0500

    error w/ depreciated packages -- Richard Li

--- devtao/server/config/db.js
+++ devtao/server/config/db.js
@@ -2,10 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
+
     console.log("✅ MongoDB Connected Successfully!");
   } catch (error) {
     console.error("❌ MongoDB Connection Failed:", error);
